399f20ca5c8f2b353ede9159fdcdf304e933f211
tartempion-sdr <mammuth4639@gmail.com>
2024-08-16 23:28:20 +0200

    instanceet pas instanciate

--- Node2D.gd
+++ Node2D.gd
@@ -4,11 +4,20 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var scene = load("res://Area2D.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+		test()
+# warning-ignore:unused_variable
+func test():
+	for n in range(0, 19):
+		
+		
+		var instance_plateau = scene.instance()
+		add_child(instance_plateau)
+
+	 # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
