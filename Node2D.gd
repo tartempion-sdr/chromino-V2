3efa63470a4c2c471938d58147048d12d77d2ef0
tartempion-sdr <mammuth4639@gmail.com>
2024-08-17 00:54:11 +0200

    plateau carre trait

--- Node2D.gd
+++ Node2D.gd
@@ -5,18 +5,24 @@ extends Node2D
 # var a = 2
 # var b = "text"
 var scene = load("res://Area2D.tscn")
+var y = 30
+var x = 30
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 		test()
 # warning-ignore:unused_variable
 func test():
-	for n in range(0, 19):
-		
-		
-		var instance_plateau = scene.instance()
-		add_child(instance_plateau)
+	
+	for i in range(0, 19):
+		for n in range(0, 19):
 
+			x = x + 20
+			var instance_plateau = scene.instance()
+			instance_plateau.position = Vector2(x , y)
+			add_child(instance_plateau)
+		x = 20
+		y = y + 20
 	 # Replace with function body.
 
 
