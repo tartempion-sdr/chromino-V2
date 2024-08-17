654f2eda97e28d605778609f9ca7d24b9a866a12
tartempion-sdr <mammuth4639@gmail.com>
2024-08-17 12:34:53 +0200

    input scroll

--- Node2D.gd
+++ Node2D.gd
@@ -5,17 +5,17 @@ extends Node2D
 # var a = 2
 # var b = "text"
 var scene = load("res://Area2D.tscn")
-var y = 30
-var x = 30
+var y = 40
+var x = 40
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-		test()
+		test(x, y)
 # warning-ignore:unused_variable
-func test():
+func test(x, y):
 	
-	for i in range(0, 19):
-		for n in range(0, 19):
+	for i in range(0, 79):
+		for n in range(0, 79):
 
 			x = x + 20
 			var instance_plateau = scene.instance()
@@ -31,3 +31,11 @@ func test():
 #	pass
 
 		
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_WHEEL_UP:
+			# Action pour le défilement vers le haut
+			print("Défilement vers le haut")
+		elif event.button_index == BUTTON_WHEEL_DOWN:
+			# Action pour le défilement vers le bas
+			print("Défilement vers le bas")
