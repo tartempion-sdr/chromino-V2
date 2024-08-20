52fb9792d49727744776e3097359e8238b8196dd
tartempion-sdr <mammuth4639@gmail.com>
2024-08-20 18:00:24 +0200

    recree plateau input clic gauche souris

--- Node2D.gd
+++ Node2D.gd
@@ -1,41 +1,65 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var scene = load("res://Area2D.tscn")
-var y = 40
+var scene = preload("res://Area2D.tscn")
 var x = 40
+var y = 40
 
-# Called when the node enters the scene tree for the first time.
+
+
+
 func _ready():
-		test(x, y)
-# warning-ignore:unused_variable
-func test(x, y):
 	
-	for i in range(0, 79):
-		for n in range(0, 79):
+	pos_plateau()
+	
+func pos_plateau():
+	var mouse_position = get_viewport().get_mouse_position()
+	var a = int()
+	var retour =int()
+	for i in range(0, 9):
+		for n in range(0, 9):
 
-			x = x + 20
-			var instance_plateau = scene.instance()
-			instance_plateau.position = Vector2(x , y)
-			add_child(instance_plateau)
-		x = 20
-		y = y + 20
-	 # Replace with function body.
+			
+			plateau(Vector2(x, y))
+			a = 20
+			x += 20 
+			retour = retour + a
+			
+			
+		x -= retour
+		retour =0
+		y += 20
+	
 
+func plateau(pos):
+	var instance_plateau = scene.instance()
+	instance_plateau.position = pos
+	add_child(instance_plateau)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+# Called every fr
+	pass
+	
 
-		
 func _input(event):
+	var instance_plateau = scene.instance()
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_WHEEL_UP:
 			# Action pour le défilement vers le haut
 			print("Défilement vers le haut")
+			for child in get_children():
+				if child is Area2D:
+			# Modifier la position de l'enfant
+					print("up")
 		elif event.button_index == BUTTON_WHEEL_DOWN:
 			# Action pour le défilement vers le bas
 			print("Défilement vers le bas")
+		elif event.button_index == BUTTON_LEFT and event.pressed:
+		# Action pour le clic gauche
+			print("Clic gauche")
+			var mouse_position = get_viewport().get_mouse_position()
+			print(mouse_position)
+			x = mouse_position[0]
+			y = mouse_position[1]
+			pos_plateau()
+			
