65a4a45511039bf6ca4c76ce19bd8fa9fe39a02f
tartempion-sdr <mammuth4639@gmail.com>
2024-10-09 10:54:52 +0200

    couleur preload + var liste

--- Node2D.gd
+++ Node2D.gd
@@ -1,6 +1,6 @@
 extends Node2D 
 
-var list_jetons = []
+
 var ecart = 20
 var taille = 1
 var scene = preload("res://Area2D.tscn")
@@ -9,16 +9,35 @@ var y1 = 0
 var plateau_x
 var plateau_y
 var child_2 
-
 var x = 0
 var y = 0
 var num = 2
 
+#75 chrominos classiques, 
+#5 chrominos caméléon combinant 2 couleurs différentes à chaque extrémité
+var joker = preload("res://assets/colors/joker.jpg")
+var jaune = preload("res://assets/colors/jaune.jpg")
+var vert = preload("res://assets/colors/vert.jpg")
+var bleu = preload("res://assets/colors/bleu.jpg")
+var violet = preload("res://assets/colors/violet.jpg")
+var rouge = preload("res://assets/colors/rouge.jpg")
+
+
+var list_couleurs_jetons = []
+
+var list_position_pioche_jetons = []
+var list_affihe_position_porte_jetons = []
 
 func _ready():
 
 	pos_plateau()
+	cree_jetons()
 	
+func plateau(pos):
+	var instance_plateau = scene.instance()
+	instance_plateau.position = pos
+	instance_plateau.visible = true
+	add_child(instance_plateau)
 
 func pos_plateau():
 	
@@ -39,14 +58,12 @@ func pos_plateau():
 		y += 20
 	x = 0
 	y = 0   
-	
+
 func souris_pos_plateau():
 	
 	var retourx = 0
 	var retoury = 0
 	num = 2
-	
-	
 	
 	for n in range(9):
 
@@ -71,16 +88,12 @@ func souris_pos_plateau():
 	y1 -= retoury
 	print(taille)
 
+func cree_jetons():
+	pass
 
-func plateau(pos):
-	var instance_plateau = scene.instance()
-	instance_plateau.position = pos
-	instance_plateau.visible = true
-	add_child(instance_plateau)
+func pioche_jeton_random():
+	print("random jetons")
 
-
-		
-		
 func _input(event):
 
 	if event is InputEventMouseButton:
@@ -115,3 +128,14 @@ func _input(event):
 		
 		y1 += 10
 		souris_pos_plateau()
+
+func _on_Button_pioche_button_down():
+	pioche_jeton_random()
+
+
+func _on_Button_droite_button_down():
+	pass # Replace with function body.
+
+
+func _on_Button_gauche_button_down():
+	pass # Replace with function body.
