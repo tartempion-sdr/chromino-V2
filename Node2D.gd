6ab980e0a55383bb028f7557565b16769ab5dcac
tartempion-sdr <mammuth4639@gmail.com>
2024-10-25 22:09:16 +0200

    Grand plateau 19x19 stable 2

--- Node2D.gd
+++ Node2D.gd
@@ -12,7 +12,7 @@ var child_2
 var x = 0
 var y = 0
 var num = 2
-var jeton_child = 83
+var jeton_child = 363
 var sens = ["verticale-h", "horizontale-d", "verticale-b", "horizontale-g"] 
 var nouveau
 #75 chrominos classiques, 
@@ -125,12 +125,12 @@ func _ready():
 	random.randomize()
 	pos_plateau()
 	pioche_jetons()
-	modifie_plateau(33, list_couleurs_jetons[hazard][0])
-	modifie_plateau(42, list_couleurs_jetons[hazard][1])
-	modifie_plateau(51, list_couleurs_jetons[hazard][2])
+	modifie_plateau(163, list_couleurs_jetons[hazard][0])
+	modifie_plateau(182, list_couleurs_jetons[hazard][1])
+	modifie_plateau(201, list_couleurs_jetons[hazard][2])
 	
-	var child_jeton = get_child(83)
-	print("Enfant à l'index ", " : ", child_jeton.name)
+	var child_jeton = get_child(363)
+	#print("Enfant à l'index ", " : ", child_jeton.name)
 	child_jeton.visible = false
 	
 	Globals.list_affiche_position_porte_jetons.clear()
@@ -156,8 +156,8 @@ func pos_plateau():
 	var a = 0
 	var retour = 0
 	
-	for i in range(0, 9):
-		for n in range(0, 9):
+	for i in range(0, 19):
+		for n in range(0, 19):
 			
 			plateau(Vector2(x, y))
 			a = 20
@@ -175,9 +175,9 @@ func souris_pos_plateau():
 	var retoury = 0
 	num = 2
 	
-	for n in range(9):
+	for n in range(19):
 
-		for i in range(9):
+		for i in range(19):
 			
 			# Utiliser un index pour accéder à chaque enfant individuellement
 			var child = get_child(num)
@@ -212,10 +212,10 @@ func pioche_jetons():
 	var rech = instance_jeton.get_child(0)
 	jeton_child = rech.get_instance_id()
 	add_child(instance_jeton)
-	print("id jeton " + str(jeton_child))
+	#print("id jeton " + str(jeton_child))
 	
 	
-	print(instance_jeton.name)
+	#print(instance_jeton.name)
 	Globals.affiche_jetons_piocher()
 
 	
@@ -283,7 +283,7 @@ func _input(event):
 func _on_Button_pioche_button_down():
 	
 	child_pioche_jetons()
-	print(Globals.list_affiche_position_porte_jetons)
+	#print(Globals.list_affiche_position_porte_jetons)
 	
 func _on_Button_droite_button_down():
 	
@@ -298,7 +298,7 @@ func _on_Button_droite_button_down():
 func _on_Button_gauche_button_down():
 
 	if len(Globals.list_affiche_position_porte_jetons) == 0:
-		print("pioche dabore")
+		print("pioche d'abore")
 		
 	elif len(Globals.list_affiche_position_porte_jetons) >= 0:
 	
@@ -315,7 +315,7 @@ func _on_Button_tourne_button_down():
 		
 		var target_id = Globals.list_affiche_position_porte_jetons[0][5]  # Remplace par ton ID de nœud
 		var node = find_node_by_instance_id(get_tree().root, target_id)
-		print("target" + str(target_id))
+		#print("target" + str(target_id))
 		if node:
 			
 			node.rotation_degrees += 90
@@ -328,7 +328,7 @@ func _on_Button_tourne_button_down():
 			else:
 				nouveau = 0
 			Globals.list_affiche_position_porte_jetons[0][4] = sens[nouveau]
-			print(Globals.list_affiche_position_porte_jetons[0][4])
+			#print(Globals.list_affiche_position_porte_jetons[0][4])
 			#print("tourn_n_child" + str(Globals.list_affiche_position_porte_jetons[0][5]))
 			#print("list " + str(Globals.list_affiche_position_porte_jetons))
 
